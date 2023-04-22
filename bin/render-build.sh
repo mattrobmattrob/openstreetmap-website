aaac1fc4b098ec622a9938ffae01b1fa0e727dc7
Matt Robinson <matt_rob@me.com>
2023-04-22 16:49:05 -0600

    Try again

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,14 +2,18 @@
 # exit on error
 set -o errexit
 
+# Get the Render build environment ready
+mount -o remount,rw /
+apt install sudo
+
 # Install correct dependencies
-apt-get update
-apt-get install ruby2.7 libruby2.7 ruby2.7-dev \
-                libvips-dev libxml2-dev libxslt1-dev nodejs \
-                apache2 apache2-dev build-essential git-core firefox-geckodriver \
-                postgresql postgresql-contrib libpq-dev libsasl2-dev \
-                libffi-dev libgd-dev libarchive-dev libbz2-dev yarnpkg
-gem2.7 install bundler
+sudo apt-get update
+sudo apt-get install ruby2.7 libruby2.7 ruby2.7-dev \
+                     libvips-dev libxml2-dev libxslt1-dev nodejs \
+                     apache2 apache2-dev build-essential git-core firefox-geckodriver \
+                     postgresql postgresql-contrib libpq-dev libsasl2-dev \
+                     libffi-dev libgd-dev libarchive-dev libbz2-dev yarnpkg
+sudo gem2.7 install bundler
 
 # Get Rails ready to go
 bundle install
