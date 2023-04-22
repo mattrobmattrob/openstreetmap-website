a007daf86016d1742a970a8323d28f7adc559d1b
Matt Robinson <matt_rob@me.com>
2023-04-22 16:41:04 -0600

    Add Render deployment

---
+++ bin/render-build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# Install correct dependencies
+sudo apt-get update
+sudo apt-get install ruby2.7 libruby2.7 ruby2.7-dev \
+                     libvips-dev libxml2-dev libxslt1-dev nodejs \
+                     apache2 apache2-dev build-essential git-core firefox-geckodriver \
+                     postgresql postgresql-contrib libpq-dev libsasl2-dev \
+                     libffi-dev libgd-dev libarchive-dev libbz2-dev yarnpkg
+sudo gem2.7 install bundler
+
+# Get Rails ready to go
+bundle install
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rake db:migrate
