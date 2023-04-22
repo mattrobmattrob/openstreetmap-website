60e97e30725d6cf3702588f030ff31fd5b009d45
Matt Robinson <matt_rob@me.com>
2023-04-22 16:44:36 -0600

    Remove `sudo` from `render-build.sh`

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,13 +3,13 @@
 set -o errexit
 
 # Install correct dependencies
-sudo apt-get update
-sudo apt-get install ruby2.7 libruby2.7 ruby2.7-dev \
-                     libvips-dev libxml2-dev libxslt1-dev nodejs \
-                     apache2 apache2-dev build-essential git-core firefox-geckodriver \
-                     postgresql postgresql-contrib libpq-dev libsasl2-dev \
-                     libffi-dev libgd-dev libarchive-dev libbz2-dev yarnpkg
-sudo gem2.7 install bundler
+apt-get update
+apt-get install ruby2.7 libruby2.7 ruby2.7-dev \
+                libvips-dev libxml2-dev libxslt1-dev nodejs \
+                apache2 apache2-dev build-essential git-core firefox-geckodriver \
+                postgresql postgresql-contrib libpq-dev libsasl2-dev \
+                libffi-dev libgd-dev libarchive-dev libbz2-dev yarnpkg
+gem2.7 install bundler
 
 # Get Rails ready to go
 bundle install
